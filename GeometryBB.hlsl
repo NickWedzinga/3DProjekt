2734123de5ecac1e992df8e16b4a418be54321af
asd <asd>
2016-11-22 11:43:40 +0100

    Started working on testing billboard math purely on GPU

--- GeometryBB.hlsl
+++ GeometryBB.hlsl
@@ -29,34 +29,41 @@ struct GSOutput
 };
 
 [maxvertexcount(6)]
-void GS_main(triangle GS_IN input[3], inout TriangleStream< GSOutput > output)
+void GS_main(triangle GS_IN input[3], /*point GS_IN input,*/ inout TriangleStream< GSOutput > output)
 {
 	GSOutput element;
 
-	float3 center = (1 / 3) * (input[0].pos + input[1].pos + input[2].pos);
+	/*float3 center = (1 / 3) * (input[0].pos + input[1].pos + input[2].pos);
 	float3 bbToCam = camPos - center;
 	float3 up = float3(0.0f, 1.0f, 0.0f);
 	float3 left = cross(bbToCam, up);
 	up = cross(left, bbToCam);
+	float angle = dot(bbToCam, input[0].normal);*/
 
-	/*float4 posNew[3];
-	posNew[0]= input[0].pos + left * 2 + up * 2;
-	posNew[1] = input[1].pos + left * 2 + up * 2;
-	posNew[2] = input[2].pos + left * 2 + up * 2;*/
-
-	
-	//float angle = dot(bbToCam, input[0].normal);
-
-	
-
-
+	//GS för 1 input point ist för triangel, kräver mycket mindre arbete på CPU-sidan, kan vara värt att prova impl
+	//ändra input type till point eller nåt
+	/*float3 bbToCam = camPos - input.pos;
+	float3 up = float3(0.0f, 1.0f, 0.0f);
+	float3 left = cross(bbToCam, up);
+	up = cross(left, bbToCam);
+	float4 billboard[6]; 
+	billboard[0] = input.pos + float4(left, 0.0f) + float4(up, 0.0f) * 2; //top left
+	billboard[1] = input.pos - float4(left, 0.0f) - float4(up, 0.0f) * 2; //bottom right
+	billboard[2] = input.pos + float4(left, 0.0f) - float4(up, 0.0f) * 2; //bottom left
+	billboard[3] = input.pos + float4(left, 0.0f) + float4(up, 0.0f) * 2; //top left
+	billboard[4] = input.pos - float4(left, 0.0f) + float4(up, 0.0f) * 2; //top right
+	billboard[5] = input.pos - float4(left, 0.0f) - float4(up, 0.0f) * 2; //bottom right
+	element.pos = mul(ProjMatrix, mul(ViewMatrix, mul(RotationMatrix, input.pos)));
+	element.normal = mul(ProjMatrix, mul(ViewMatrix, mul(RotationMatrix, input.pos)));
+	element.UV = input.UV;
+	element.ID = input.ID;
+	output.Append(element)
+	*/
 
 	for (uint i = 0; i < 3; i++)
 	{
 		float4 poss = input[i].pos;
-
-
-		
+	
 		//poss = mul(WorldMatrix, poss);
 		poss = mul(RotationMatrix, poss);
 		poss = mul(ViewMatrix, poss);
